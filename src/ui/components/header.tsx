01315b1a6a0631f7bc924de2bdaa4bdadd3797b0
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-03-01 18:06:52 -0300

    feat: add header component

---
+++ src/ui/components/header.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/helpers/cn'
+import { View, ViewProps } from 'react-native'
+import { Typography } from '../atoms/typography'
+
+export type RootProps = ViewProps
+export type TitleProps = Parameters<typeof Typography.Title>[0]
+
+function Root(props: RootProps) {
+	const { className, ...rest } = props
+	return (
+		<View
+			className={cn(
+				'w-full flex-row items-center justify-between p-4',
+				className
+			)}
+			{...rest}
+		/>
+	)
+}
+
+function Title(props: TitleProps) {
+	const { className, ...rest } = props
+	return (
+		<Typography.Title className={cn('max-w-[200px]', className)} {...rest} />
+	)
+}
+
+export const Header = {
+	Root,
+	Title,
+}

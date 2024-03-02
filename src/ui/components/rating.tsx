b031dc695c1df24eaaa748bafacbf7eda8f07135
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-03-02 18:57:01 -0300

    feat: add rating component

---
+++ src/ui/components/rating.tsx
@@ -0,0 +1,22 @@
+import { AirbnbRating } from 'react-native-ratings'
+
+export type RatingProps = {
+	value: number
+	readonly?: boolean
+	onFinishRating?: (rating: number) => void
+}
+
+export function Rating(props: RatingProps) {
+	const { value, onFinishRating, readonly } = props
+	return (
+		<AirbnbRating
+			count={5}
+			defaultRating={value}
+			size={20}
+			selectedColor="#000000"
+			showRating={false}
+			isDisabled={readonly}
+			onFinishRating={onFinishRating}
+		/>
+	)
+}

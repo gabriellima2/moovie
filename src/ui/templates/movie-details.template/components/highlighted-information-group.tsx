fd876d4b3531c112bd8f1fe6ee8bf92b4a9d7e17
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-04-06 21:51:30 -0300

    chore: define styles for movie-details template

---
+++ src/ui/templates/movie-details.template/components/highlighted-information-group.tsx
@@ -0,0 +1,30 @@
+import { View } from 'react-native'
+import { Typography } from '@/ui/atoms/typography'
+
+export type HighlightedInformationGroupProps = {
+	label: string
+	values: string | string[]
+}
+
+export function HighlightedInformationGroup(
+	props: HighlightedInformationGroupProps
+) {
+	const { label, values } = props
+	const isAnArrayOfValues = Array.isArray(values)
+	return (
+		<View className="flex-row gap-x-2 items-center flex-wrap mt-2">
+			<Typography.Paragraph>{label}:</Typography.Paragraph>
+			{isAnArrayOfValues ? (
+				values.map((value) => (
+					<Typography.Label key={value} className="bg-zinc-200 p-2 rounded-lg">
+						{value.trim()}
+					</Typography.Label>
+				))
+			) : (
+				<Typography.Label className="bg-zinc-200 p-2 rounded-lg">
+					{values.trim()}
+				</Typography.Label>
+			)}
+		</View>
+	)
+}
--- src/ui/templates/movie-details.template/movie-details.template.tsx
+++ src/ui/templates/movie-details.template/movie-details.template.tsx
@@ -6,6 +6,9 @@ import { Section } from '@/ui/components/section'
 import { ErrorText } from '@/ui/atoms/error-text'
 
 import { useGetMovieDetails } from './hooks/use-get-movie-details'
+import { CalendarDays, Clock, Star } from 'lucide-react-native'
+import colors from 'tailwindcss/colors'
+import { HighlightedInformationGroup } from './components/highlighted-information-group'
 
 export type MovieDetailsTemplateProps = {
 	name: string
@@ -15,49 +18,72 @@ export function MovieDetailsTemplate(props: MovieDetailsTemplateProps) {
 	const { name } = props
 	const { data, error, isLoading } = useGetMovieDetails(name)
 	return (
-		<ScrollView className="flex-1">
+		<ScrollView className="flex-1 mb-4">
 			{isLoading && <ActivityIndicator />}
 			{error && <ErrorText text={error.message} />}
 			{data && (
-				<View className="flex-1">
-					<View className="px-4">
+				<View className="flex-1 mt-4">
+					<View className="px-4 mb-4">
 						<Image
 							source={{ uri: data.Poster }}
 							style={{ width: '100%', height: undefined, aspectRatio: 4 / 4 }}
 							className="rounded-2xl"
 						/>
 					</View>
-					<Section.Root>
+					<Section.Root className="gap-y-4 mb-4">
 						<Typography.Title>{data.Title}</Typography.Title>
 						<Typography.Paragraph>{data.Plot}</Typography.Paragraph>
-						<View>
-							<Typography.Paragraph>{data.Released}</Typography.Paragraph>
-							<Typography.Paragraph>{data.Runtime}</Typography.Paragraph>
-							<Typography.Paragraph>{data.imdbRating}</Typography.Paragraph>
+						<View className="flex-row justify-between flex-wrap py-2">
+							<View className="flex-row gap-x-2 items-center">
+								<Clock color={colors.black} size={20} />
+								<Typography.Paragraph>{data.Runtime}</Typography.Paragraph>
+							</View>
+							<View className="flex-row gap-x-2 items-center">
+								<CalendarDays color={colors.black} size={20} />
+								<Typography.Paragraph>{data.Released}</Typography.Paragraph>
+							</View>
+							<View className="flex-row items-center">
+								<Star
+									fill={colors.black}
+									color={colors.black}
+									size={20}
+									className="mr-2"
+								/>
+								<Typography.Paragraph>{data.imdbRating}/</Typography.Paragraph>
+								<Typography.Small>10</Typography.Small>
+							</View>
 						</View>
 						<View>
-							{data.Genre.split(',').map((genre) => (
-								<Typography.Paragraph key={genre}>
-									{genre.trim()}
-								</Typography.Paragraph>
-							))}
-							<Typography.Paragraph>{data.Director}</Typography.Paragraph>
+							<HighlightedInformationGroup
+								label="Genre"
+								values={data.Genre.split(',')}
+							/>
+							<HighlightedInformationGroup
+								label="Director"
+								values={data.Director}
+							/>
 						</View>
 					</Section.Root>
 					<Section.Root>
 						<Section.Title>Reviews</Section.Title>
-						{data.reviews.map((review) => (
-							<View key={review.id} className="mb-4">
-								<ReviewPreview
-									id={review.id}
-									movieName={review.movie_name}
-									likes={review.likes_id}
-									userID={review.user_id}
-									description={review.description}
-									rating={review.rating}
-								/>
-							</View>
-						))}
+						{!data.reviews.length ? (
+							<Typography.Paragraph className="text-center">
+								There are no reviews for this movie at the moment
+							</Typography.Paragraph>
+						) : (
+							data.reviews.map((review) => (
+								<View key={review.id} className="mb-4">
+									<ReviewPreview
+										id={review.id}
+										movieName={review.movie_name}
+										likes={review.likes_id}
+										userID={review.user_id}
+										description={review.description}
+										rating={review.rating}
+									/>
+								</View>
+							))
+						)}
 					</Section.Root>
 				</View>
 			)}

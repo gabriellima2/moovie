b3d3fbbc2e01317e45afcbdd476883840cb7b211
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-07-27 16:47:43 -0300

    fix: type errors

--- src/ui/templates/home.template/contexts/home.context/@types/home-context-values.ts
+++ src/ui/templates/home.template/contexts/home.context/@types/home-context-values.ts
@@ -4,6 +4,6 @@ import type { RecommendationListDTO } from '@/dtos/recommendation-list.dto'
 import type { ReviewDTO } from '@/dtos/review.dto'
 
 export type HomeContextValues = {
-	reviews: UseQueryResult<ReviewDTO[], Error>
-	recommendations: UseQueryResult<RecommendationListDTO[], Error>
+	reviews: UseQueryResult<ReviewDTO[] | undefined, Error>
+	recommendations: UseQueryResult<RecommendationListDTO[] | undefined, Error>
 }

d5c3a847588083639ac99ad9abdb06435a12cb0b
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-08-10 14:59:01 -0300

    feat: save selected recommendations-list options on checked change

--- src/hooks/use-boolean.ts
+++ src/hooks/use-boolean.ts
@@ -14,6 +14,7 @@ export function useBoolean(initialValue?: boolean) {
 	}
 	function toggle() {
 		setState((prevState) => !prevState)
+		return !state
 	}
 
 	return {
--- src/ui/templates/movie-details.template/components/add-to-list-modal/index.tsx
+++ src/ui/templates/movie-details.template/components/add-to-list-modal/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { TouchableHighlight, TouchableOpacity, View } from 'react-native'
+import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
 import { Check, Plus } from 'lucide-react-native'
 import colors from 'tailwindcss/colors'
 
@@ -9,35 +10,40 @@ import { Typography } from '@/ui/atoms/typography'
 import { useGetRecommendationsListByUserId } from '@/hooks/use-get-recommendations-list-by-user-id'
 import { useAuthenticationStore } from '@/store/authentication.store/authentication.store'
 import { useMovieDetailsContext } from '../../contexts/movie-details.context'
+import { useBoolean } from '@/hooks/use-boolean'
 
-import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
 import { cn } from '@/helpers/cn'
 
 type RecommendationsListOptionProps = {
+	id: string
 	title: string
+	onCheckedChange: (id: string, checked: boolean) => void
 }
 
 function RecommendationsListOption(props: RecommendationsListOptionProps) {
-	const { title } = props
-	const [isChecked, setIsChecked] = useState(false)
+	const { id, title, onCheckedChange } = props
+	const { value: isChecked, toggle: toggleIsChecked } = useBoolean(false)
+
+	function handleToggleIsChecked() {
+		const nextValue = toggleIsChecked()
+		onCheckedChange(id, nextValue)
+	}
+
 	return (
 		<TouchableHighlight
 			underlayColor={colors.zinc[100]}
-			onPress={() => setIsChecked((prevState) => !prevState)}
+			onPress={handleToggleIsChecked}
 			className="flex-1 p-5"
 		>
 			<View className="flex-row items-center justify-start">
 				<View
 					className={cn(
 						'border border-zinc-400 w-5 h-5 items-center justify-center rounded-md',
-						{
-							'bg-black border-0': isChecked,
-						}
+						{ 'bg-black border-0': isChecked }
 					)}
 				>
 					{isChecked && <Check color={colors.white} size={16} />}
 				</View>
-
 				<Typography.Subtitle className="text-base ml-3">
 					{title}
 				</Typography.Subtitle>
@@ -50,13 +56,34 @@ export function AddToListModal() {
 	const { closeAddToListModal } = useMovieDetailsContext()
 	const user = useAuthenticationStore((state) => state.user)
 	const { recommendationsList } = useGetRecommendationsListByUserId(user!.uid)
+	const [selectedListOptions, setSelectedListOptions] = useState<string[]>([])
+
+	function handleCheckedChange(id: string, checked: boolean) {
+		if (checked) {
+			setSelectedListOptions((prevState) => [...prevState, id])
+			return
+		}
+		setSelectedListOptions((prevState) =>
+			prevState.filter((item) => item !== id)
+		)
+	}
+
+	function handleConfirmPress() {
+		if (!selectedListOptions.length) return
+		console.log(selectedListOptions)
+	}
+
 	return (
 		<BottomSheet onClose={closeAddToListModal} withoutScrollView>
 			<BottomSheetFlatList
 				data={recommendationsList}
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => (
-					<RecommendationsListOption title={item.title} />
+					<RecommendationsListOption
+						id={item.id}
+						title={item.title}
+						onCheckedChange={handleCheckedChange}
+					/>
 				)}
 				contentContainerStyle={{ paddingTop: 20 }}
 				ListHeaderComponent={() => (
@@ -76,6 +103,7 @@ export function AddToListModal() {
 				ListFooterComponent={() => (
 					<TouchableOpacity
 						activeOpacity={0.8}
+						onPress={handleConfirmPress}
 						className="py-5 items-center justify-center"
 					>
 						<Typography.Label>Confirm</Typography.Label>
